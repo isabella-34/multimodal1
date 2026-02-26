28876be2df75a521cee0d3548974be416cdd9770
isabella-34 <isamolial34@gmail.com>
2026-02-26 10:03:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from  PIL import Image
 
 st.title("Esta es mi primera app en la nube")
+st.header("En este espacio comienzo a desarrollar mis apps de Interfaces Multimodales")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('poster.png')
+st.image(image, caption='Interfaces Multimodales')
