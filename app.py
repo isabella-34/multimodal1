9bf6535d29ea71912d3ecfd7d8e6112910c36520
isabella-34 <isamolial34@gmail.com>
2026-02-26 10:03:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from  PIL import Image
 st.title("Esta es mi primera app en la nube")
 st.header("En este espacio comienzo a desarrollar mis apps de Interfaces Multimodales")
 st.write("Facilmente puedo realizar backend y frontend.")
-image = Image.open('poster.png')
+image = Image.open('poster')
 st.image(image, caption='Interfaces Multimodales')
