f6a10ba752f222378b209dbb53c9801180daa556
isabella-34 <isamolial34@gmail.com>
2026-02-26 10:24:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,15 @@ st.image(image, caption='Ilustración')
 
 texto = st.text_input('Escribe algo', 'Este es mi texto')
 st.write('El texto escrito es', texto)
+
+st.subheader("Ahora usemos 2 columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  resp = st.checkbox('Estoy de acuerdo')
+  if resp:
+    st.write('¡Correcto!')
+    
