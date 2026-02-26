111d300955778a655e9ca25efe7a2391bc3b0f92
isabella-34 <isamolial34@gmail.com>
2026-02-26 10:16:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ st.title("Esta es mi primera app en la nube")
 st.header("En este espacio comienzo a desarrollar mis apps de Interfaces Multimodales")
 st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('heart.jpeg')
-st.image(image, caption='Interfaces Multimodales')
+st.image(image, caption='Ilustración')
+
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
