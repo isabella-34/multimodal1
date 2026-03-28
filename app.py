ddc639c732e96f8eae44e96423f98cad5a1a6705
isabella-34 <isamolial34@gmail.com>
2026-03-28 15:29:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,29 +8,29 @@ image = Image.open('heart.jpeg')
 st.image(image, caption='Ilustración')
 
 
-texto = st.text_input('Escribe algo', 'Este es mi texto')
-st.write('El texto escrito es', texto)
+texto = st.text_input('¿Qué opinas de mi ilustración?', 'Opino que...')
+st.write('Piensas que ', texto)
 
 st.subheader("Ahora usemos 2 columnas")
 
 col1, col2 = st.columns(2)
 
 with col1:
-  st.subheader("Esta es la primera columna")
-  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  st.subheader("Señala si crees correcta la afirmación")
+  st.write("Las ilustraciones comunican sentimientos de manera diferente, aegún quien las mire.")
   resp = st.checkbox('Estoy de acuerdo')
   if resp:
-    st.write('¡Correcto!')
+    st.write('¡Así es!')
 
 with col2:
-  st.subheader("Esta es la segunda columna")
-  modo = st.radio("¿Qué modalidad es la principal en tu interfaz?", ('Visual', 'Auditiva', 'Táctil'))
-  if modo == 'Visual':
-    st.write("La vista es fundamental para tu interfaz")
-  if modo == 'Auditiva':
-    st.write("La audición es fundamental para tu interfaz")
-  if modo == 'Táctil':
-    st.write("El tacto es fundamental para tu interfaz")
+  st.subheader("Tus preferencias")
+  modo = st.radio("¿Qué movimiento artistico te gusta más?", ('Expresionismo', 'Surrealismo', 'Dadaísmo'))
+  if modo == 'Expresionismo':
+    st.write("Te interesa la expresión emocional intensa.")
+  if modo == 'Surrealismo':
+    st.write("Te interesa explorar del inconsciente y los sueños.")
+  if modo == 'Dadaísmo':
+    st.write("Te interesa lo anti-arte, absurdo y protesta.")
 
 st.subheader("Uso de botones")
 if st.button('Presiona el botón'):
